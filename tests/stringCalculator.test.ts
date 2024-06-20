47b88b25c05918d786f58a3cefbcaac6ef6718f4
SuryaNesanuru <156491301+SuryaNesanuru3@users.noreply.github.com>
2024-06-21 04:00:55 +0530

    Test: Add test for negative numbers

--- tests/stringCalculator.test.ts
+++ tests/stringCalculator.test.ts
@@ -1,4 +1,4 @@
-//import { add } from '../src/stringCalculator';
+import { add } from '../src/stringCalculator';
 
 //test('should handle new lines between numbers', () => {
  // expect(add('1\n2,3')).toBe(6);
@@ -28,25 +28,15 @@ test('should list all negative numbers in the exception message', () => {
 //  expect(add('//;\n1;2;3')).toBe(6);
 //});
 
-// src/stringCalculator.ts
-export function add(numbers: string): number {
-  if (numbers === '') return 0;
+// tests/stringCalculator.test.ts
 
-  let delimiter = ',';
-  if (numbers.startsWith('//')) {
-    const delimiterEndIndex = numbers.indexOf('\n');
-    delimiter = numbers.substring(2, delimiterEndIndex);
-    numbers = numbers.substring(delimiterEndIndex + 1);
-  }
+test('should throw an exception for negative numbers', () => {
+  expect(() => add('1,-2,3')).toThrow('negative numbers not allowed: -2');
+});
 
-  numbers = numbers.replace(/\n/g, delimiter);
-  const nums = numbers.split(delimiter).map(num => parseInt(num, 10));
-  const negatives = nums.filter(num => num < 0);
-  if (negatives.length > 0) {
-    throw new Error(`negative numbers not allowed: ${negatives.join(',')}`);
-  }
-  return nums.reduce((sum, num) => sum + num, 0);
-}
+test('should list all negative numbers in the exception message', () => {
+  expect(() => add('1,-2,-3')).toThrow('negative numbers not allowed: -2,-3');
+});
 
 
 
