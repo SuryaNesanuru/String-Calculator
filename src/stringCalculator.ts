34b1f799e4360f2ff1e1e0894b22c4a3a312bf0b
SuryaNesanuru <156491301+SuryaNesanuru3@users.noreply.github.com>
2024-06-21 04:00:07 +0530

    Feat: Handle negative numbers with exceptions

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -56,6 +56,28 @@
                 .reduce((sum, num) => sum + num, 0);
 }*/
 
+export function add(numbers: string): number {
+    if (numbers === '') return 0;
+  
+    let delimiter = ',';
+    if (numbers.startsWith('//')) {
+      const delimiterEndIndex = numbers.indexOf('\n');
+      delimiter = numbers.substring(2, delimiterEndIndex);
+      numbers = numbers.substring(delimiterEndIndex + 1);
+    }
+  
+    const escapedDelimiter = delimiter.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+    const delimiterRegex = new RegExp(escapedDelimiter, 'g');
+    numbers = numbers.replace(/\n/g, delimiter).replace(delimiterRegex, ',');
+    const nums = numbers.split(',').map(num => parseInt(num, 10));
+    const negatives = nums.filter(num => num < 0);
+    if (negatives.length > 0) {
+      throw new Error(`negative numbers not allowed: ${negatives.join(',')}`);
+    }
+    return nums.reduce((sum, num) => sum + num, 0);
+  }
+  
+  
 
 
 
