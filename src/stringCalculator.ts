a7ca6c0597d6609d29a8312854168c02c3c29686
SuryaNesanuru <156491301+SuryaNesanuru3@users.noreply.github.com>
2024-06-21 03:40:14 +0530

    Test: Add test for negative numbers

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -6,7 +6,7 @@
                   .reduce((sum, num) => sum + num, 0);
   }*/
 
-export function add(numbers: string): number {
+/*export function add(numbers: string): number {
   if (numbers === '') return 0;
   
   let delimiter = ',';
@@ -20,6 +20,26 @@ export function add(numbers: string): number {
   return numbers.split(delimiter)
                 .map(num => parseInt(num, 10))
                 .reduce((sum, num) => sum + num, 0);
-}
+}*/
+
+export function add(numbers: string): number {
+    if (numbers === '') return 0;
+    
+    let delimiter = ',';
+    if (numbers.startsWith('//')) {
+      const delimiterEndIndex = numbers.indexOf('\n');
+      delimiter = numbers.substring(2, delimiterEndIndex);
+      numbers = numbers.substring(delimiterEndIndex + 1);
+    }
+    
+    numbers = numbers.replace(/\n/g, delimiter);
+    const nums = numbers.split(delimiter).map(num => parseInt(num, 10));
+    const negatives = nums.filter(num => num < 0);
+    if (negatives.length > 0) {
+      throw new Error(`negative numbers not allowed: ${negatives.join(',')}`);
+    }
+    return nums.reduce((sum, num) => sum + num, 0);
+  }
+  
 
   
--- tests/stringCalculator.test.ts
+++ tests/stringCalculator.test.ts
@@ -4,9 +4,18 @@ import { add } from '../src/stringCalculator';
  // expect(add('1\n2,3')).toBe(6);
 //});
 
-test('should support different delimiters', () => {
+/*test('should support different delimiters', () => {
   expect(add('//;\n1;2')).toBe(3);
+});*/
+
+test('should throw an exception for negative numbers', () => {
+  expect(() => add('1,-2,3')).toThrow('negative numbers not allowed: -2');
 });
+
+test('should list all negative numbers in the exception message', () => {
+  expect(() => add('1,-2,-3')).toThrow('negative numbers not allowed: -2,-3');
+});
+
 
 
 
