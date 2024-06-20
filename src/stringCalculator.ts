d8394a86edb71121a7ec07f1e747e438222f714b
SuryaNesanuru <156491301+SuryaNesanuru3@users.noreply.github.com>
2024-06-21 03:37:58 +0530

    Feat: Support different delimiters

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,8 +1,25 @@
-export function add(numbers: string): number {
+/*export function add(numbers: string): number {
     if (numbers === '') return 0;
     numbers = numbers.replace(/\n/g, ',');
     return numbers.split(',')
                   .map(num => parseInt(num, 10))
                   .reduce((sum, num) => sum + num, 0);
+  }*/
+
+export function add(numbers: string): number {
+  if (numbers === '') return 0;
+  
+  let delimiter = ',';
+  if (numbers.startsWith('//')) {
+    const delimiterEndIndex = numbers.indexOf('\n');
+    delimiter = numbers.substring(2, delimiterEndIndex);
+    numbers = numbers.substring(delimiterEndIndex + 1);
   }
+  
+  numbers = numbers.replace(/\n/g, delimiter);
+  return numbers.split(delimiter)
+                .map(num => parseInt(num, 10))
+                .reduce((sum, num) => sum + num, 0);
+}
+
   
